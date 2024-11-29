1017d385429c5c93cd9c0030759823b8254604c9
sebastian lozano <sebaslozano708@gmail.com>
2024-11-29 17:46:31 -0500

    fixed protected routes to avoid user access the 'sign in' and 'sign up' form once they are authenticated

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
-
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
+import SquareLoader from "../components/SquareLoader";
 
 
 export default function Auth() {
+
+  const { isLoading, isAuthenticated } = useUserContext();
+
+
+  if(isLoading) return <div className="flex justify-center items-center w-full min-h-[92vh]" >
+    <SquareLoader />
+  </div>
+
+  if(isAuthenticated) return <Navigate to="/gallery" replace />
+
   return (
     <main className="flex justify-center items-center p-6 w-full min-h-[92vh] bg-gray-100" >
       <Outlet />
